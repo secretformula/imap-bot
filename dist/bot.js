13ee6816b8ee63418c442eb133b2b25a292f94fa
Nigil Lee <nigil.lee@gmail.com>
2014-07-04 16:43:52 -0400

    basic iding of new messages

--- dist/bot.js
+++ dist/bot.js
@@ -1,7 +1,48 @@
 (function() {
+  var Imap, configFileContents, daemonConfig, fs, mailboxConnections, _;
+
   require('source-map-support').install();
 
-  console.lg('test output');
+  fs = require('fs');
+
+  Imap = require('imap');
+
+  _ = require('underscore');
+
+  configFileContents = fs.readFileSync('/home/nigil/.imapbotrc.json');
+
+  daemonConfig = JSON.parse(configFileContents);
+
+  mailboxConnections = [];
+
+  _.each(daemonConfig.mailboxes, function(mailbox) {
+    var imapConnection;
+    console.dir(mailbox);
+    imapConnection = new Imap({
+      user: mailbox.user,
+      password: mailbox.password,
+      host: mailbox.hostname,
+      port: mailbox.port,
+      tls: mailbox.secure
+    });
+    imapConnection.once('ready', function() {
+      console.log('imap is ready');
+      return imapConnection.openBox('INBOX', true, function(err, box) {
+        if (err) {
+          throw err;
+        }
+        console.log('inbox open');
+        return imapConnection.search(['UNSEEN', ['SINCE', 'July 3, 2014']], function(err, results) {
+          if (err) {
+            throw err;
+          }
+          return console.dir(results);
+        });
+      });
+    });
+    imapConnection.connect();
+    return mailboxConnections.push(imapConnection);
+  });
 
 }).call(this);
 
--- src/bot.coffee
+++ src/bot.coffee
@@ -1,1 +1,35 @@
 require('source-map-support').install()
+
+fs = require 'fs'
+Imap = require 'imap'
+_ = require 'underscore'
+
+configFileContents = fs.readFileSync '/home/nigil/.imapbotrc.json'
+daemonConfig = JSON.parse configFileContents
+
+mailboxConnections = []
+
+_.each daemonConfig.mailboxes, (mailbox) ->
+  console.dir mailbox
+  imapConnection = new Imap(
+    user: mailbox.user
+    password: mailbox.password
+    host: mailbox.hostname
+    port: mailbox.port
+    tls: mailbox.secure
+  )
+
+  imapConnection.once 'ready', () ->
+    console.log 'imap is ready'
+    imapConnection.openBox 'INBOX', true,  (err, box) ->
+      if err then throw err
+      console.log 'inbox open'
+      imapConnection.search ['UNSEEN', ['SINCE', 'July 3, 2014']], \
+      (err, results) ->
+        if err then throw err
+        console.dir results
+
+  imapConnection.connect()
+  mailboxConnections.push imapConnection
+
+
